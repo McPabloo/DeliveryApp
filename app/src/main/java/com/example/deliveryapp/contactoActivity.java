2aa75d74c84d321e6acf2bb30c4b7633d04b0721
Ana <estk11.bnbn@gmail.com>
2023-06-06 18:43:40 -0600

    actualizacion contact

--- app/src/main/java/com/example/deliveryapp/contactoActivity.java
+++ app/src/main/java/com/example/deliveryapp/contactoActivity.java
@@ -12,75 +12,57 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.deliveryapp.ui.slideshow.UserCreditAct;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class contactoActivity extends AppCompatActivity {
-    EditText name, interested, message;
-    String M_ID;
-    FirebaseAuth firebaseAuth;
-
-    ContactRegisterModel newContact = new ContactRegisterModel();
+    EditText regName, regInterested, regMessage;
+    Button btnCreate;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference userRef = database.getReference("users");
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+        regName = findViewById(R.id.editTxt_Name);
+        regInterested = findViewById(R.id.editTxt_Interests);
+        regMessage = findViewById(R.id.editTxt_Message);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        M_ID = firebaseAuth.getCurrentUser().getUid();
-        Toast.makeText(this, "<"+M_ID+">", Toast.LENGTH_SHORT).show();
-
-        name = findViewById(R.id.editTxt_Name);
-        interested = findViewById(R.id.editTxt_Interests);
-        message = findViewById(R.id.editTxt_Message);
-
-        Button saveBtn = findViewById(R.id.btn_sendContact);
-
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        btnCreate = findViewById(R.id.btn_sendContact);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("Contact");
-                try {
-                    if (TextUtils.isEmpty(name.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Please enter name", Toast.LENGTH_SHORT).show();
-                    else if (TextUtils.isEmpty(interested.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Please enter interested", Toast.LENGTH_SHORT).show();
-                    else if (TextUtils.isEmpty(message.getText().toString()))
-                        Toast.makeText(getApplicationContext(), "Please enter message", Toast.LENGTH_SHORT).show();
-                    else {
-                    newContact.setName(name.getText().toString());
-                    newContact.setInterested(interested.getText().toString());
-                    newContact.setMessage(message.getText().toString());
+                String nameC = regName.getText().toString();
+                String interestedC = regInterested.getText().toString();
+                String messageC = regMessage.getText().toString();
 
-                        Log.d("Firebase", "Attempting to add data to Firebase");
-                        dbRef.child(M_ID).setValue(newContact)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                if (!TextUtils.isEmpty(nameC) && !TextUtils.isEmpty(interestedC)) {
+                    String reviewId = userRef.child(uid).child("reviews").push().getKey();
+
+                    HashMap<String, Object> reviewMap = new HashMap<>();
+                    reviewMap.put("name", nameC);
+                    reviewMap.put("interested", interestedC);
+                    reviewMap.put("message", messageC);
+
+                    userRef.child(uid).child("reviews").child(reviewId).setValue(reviewMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
-                                public void onSuccess(Void unused) {
-                                    Log.d("Firebase", "Data added successfully");
-                                    Toast.makeText(contactoActivity.this, "Feedback Added...", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(contactoActivity.this, MainActivity2.class));
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.e("Firebase", "Error adding data to Firebase: " + e.getMessage());
-                                    Toast.makeText(contactoActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Review added", Toast.LENGTH_SHORT).show();
+                                    } else
+                                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
                                 }
                             });
-                    }
-                } catch(NumberFormatException e) {
-                    Log.e("Firebase", "Error: " + e.toString());
-                    Toast.makeText(getApplicationContext(), "Error: " + e.toString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter name, message and interested", Toast.LENGTH_SHORT).show();
                 }
             }
         });
